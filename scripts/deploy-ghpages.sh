9fe96c54be85cabd5654ec345609403ab7394551
Nathan Flew <nath@nathf.com>
2017-07-25 22:06:56 +1000

    Build outside, copy in

--- scripts/deploy-ghpages.sh
+++ scripts/deploy-ghpages.sh
@@ -11,8 +11,9 @@ set -e
 pwd
 remote=$(git config remote.origin.url)
 
-# change to build directory from middleman
-cd build
+# make a directory to put the gp-pages branch
+mkdir gh-pages-branch
+cd gh-pages-branch
 # now lets setup a new repo so we can update the gh-pages branch
 git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
 git config --global user.name "$GH_NAME" > /dev/null 2>&1
@@ -31,7 +32,8 @@ else
     git checkout --orphan gh-pages
 fi
 
-bundle exec middleman build
+# copy over or recompile the new site
+cp -a "../build/." .
 
 # stage any changes and new files
 git add -A
@@ -39,7 +41,9 @@ git add -A
 git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
 # and push, but send any output to /dev/null to hide anything sensitive
 git push --force --quiet origin gh-pages
-
-
+# go back to where we started and remove the gh-pages git repo we made and used
+# for deployment
+cd ..
+rm -rf gh-pages-branch
 
 echo "Finished Deployment!"
