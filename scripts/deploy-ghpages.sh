c59de123e8c18c03e84c3544358e7f34f20f0153
Nathan Flew <nath@nathf.com>
2017-07-26 11:34:20 +1000

    Deploy pages to master, serve from there

--- scripts/deploy-ghpages.sh
+++ scripts/deploy-ghpages.sh
@@ -22,14 +22,14 @@ git remote add --fetch origin "$remote"
 
 
 # switch into the the gh-pages branch
-if git rev-parse --verify origin/gh-pages > /dev/null 2>&1
+if git rev-parse --verify origin/master > /dev/null 2>&1
 then
-    git checkout gh-pages
+    git checkout master
     # delete any old site as we are going to replace it
     # Note: this explodes if there aren't any, so moving it here for now
     git rm -rf .
 else
-    git checkout --orphan gh-pages
+    git checkout --orphan master
 fi
 
 # copy over or recompile the new site
@@ -40,7 +40,7 @@ git add -A
 # now commit, ignoring branch gh-pages doesn't seem to work, so trying skip
 git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
 # and push, but send any output to /dev/null to hide anything sensitive
-git push --force --quiet origin gh-pages
+git push --force --quiet origin master
 # go back to where we started and remove the gh-pages git repo we made and used
 # for deployment
 cd ..
